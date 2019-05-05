994e44efe0b7932a406e36df1a45f3dd731e1e33
Zehao Huang <zehao.sean.huang@gmail.com>
2019-05-05 00:52:49 -0700

    homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="home">
-    <h1><Timer :auto-start="true" :default-val="1555570800000" /></h1>
-    <!--<img alt="xhw" src="../assets/profile.png">-->
+  <div id="home">
+    <div id="home-background-image" class="text-center">
+      <h1 id="home-background-counter" class="display-1">
+        <Timer :auto-start="true" :default-val="1555570800000"/>
+      </h1>
+    </div>
   </div>
 </template>
 
@@ -16,3 +19,28 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe');
+
+  #home-background-image {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-image: url("../assets/home-background.jpg");
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+    color: white;
+    text-align: center;
+  }
+
+  #home-background-counter {
+    position: relative;
+    top: 50%;
+    color: rgba(255,255,255,0.75);
+    font-family: 'ZCOOL KuaiLe', serif;
+  }
+</style>
